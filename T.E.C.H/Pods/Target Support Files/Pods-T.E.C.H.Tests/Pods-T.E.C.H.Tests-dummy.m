c633fe3c032457915ebda90932ebbf940b639e5f
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2016-06-21 23:29:44 -0400

    Assets+Basic UI
    - Added main assets
    - Begin working on story board

---
+++ T.E.C.H/Pods/Target Support Files/Pods-T.E.C.H.Tests/Pods-T.E.C.H.Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_T_E_C_H_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_T_E_C_H_Tests
+@end
---
+++ T.E.C.H/Pods/Target Support Files/Pods-T.E.C.H.UITests/Pods-T.E.C.H.UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_T_E_C_H_UITests : NSObject
+@end
+@implementation PodsDummy_Pods_T_E_C_H_UITests
+@end
